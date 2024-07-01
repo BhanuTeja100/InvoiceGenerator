1030759f99d04facc833fb3a1a3c6d4a6c8caa7e
BhanuTeja100 <bhanuteja1001@gmail.com>
2024-07-02 02:22:13 +0530

    commit again

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Css file for styling app component
+import './App.css';
+import Input from './components/input.js';
+
+function App() {
+  return (
+    <div className="App">
+      {/* The main input component where user details are entered */}
+      <Input />
+    </div>
+  );
+}
+
+export default App;
